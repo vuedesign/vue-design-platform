02bb9a723be998b4181e90d94e5d702bae4e8a2c
n.see <gooarter@gmail.com>
2022-06-08 22:39:06 +0800

    feat: 修复message-box 与 message 样式导入问题，临时方案

--- packages/admin/src/main.ts
+++ packages/admin/src/main.ts
@@ -1,6 +1,7 @@
 import { router, store, http, interceptors } from '@/core';
 import App from '@/App.vue';
 import { Plugin } from 'vue';
+// 修复message-box 与 message 样式导入问题，临时方案
 import '@vue-design/theme-2n-demands/message-box/scss';
 import '@vue-design/theme-2n-demands/message/scss';
 
