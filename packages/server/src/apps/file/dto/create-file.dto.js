4fdd506ae98d20a3511f467095142006f367ea64
n.see <gooarter@gmail.com>
2022-06-12 14:24:41 +0800

    feat: 删除js文件

--- packages/server/src/apps/file/dto/create-file.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.CreateFileDto = void 0;
-var CreateFileDto = /** @class */ (function () {
-    function CreateFileDto() {
-    }
-    return CreateFileDto;
-}());
-exports.CreateFileDto = CreateFileDto;
--- packages/server/src/apps/navigation/entities/navigation.entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Navigation = void 0;
-var Navigation = /** @class */ (function () {
-    function Navigation() {
-    }
-    return Navigation;
-}());
-exports.Navigation = Navigation;
