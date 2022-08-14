42fa6e3ae914e5f34e91f053b31bb1aba94684db
n.see <gooarter@gmail.com>
2022-08-14 17:20:45 +0800

    feat: jwt 过期时间 2d

--- packages/server-web/src/modules/auth/auth.module.ts
+++ packages/server-web/src/modules/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '2d' },
     }),
     BaseMicroserviceModule,
   ],
