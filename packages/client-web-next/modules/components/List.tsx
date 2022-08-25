cdd37501a594db82d94c5ff2dd8d0ea075db9dd9
n.see <gooarter@gmail.com>
2022-08-25 23:14:32 +0800

    feat: more

--- packages/client-web-next/modules/components/List.tsx
+++ packages/client-web-next/modules/components/List.tsx
@@ -2,50 +2,55 @@ import { useContext } from 'react';
 import Link from 'next/link';
 import styles from '../styles/List.module.scss';
 import { useSitesQuery } from '../redux/services/siteApi';
-// import { Pagination } from '../pages/siteDetail/components/Pagination';
+import { Pagination } from './Pagination';
 import Item from './Item';
 
 type ListProps = {
   type: string;
+  page: number;
+  size: number;
+  total: number;
 };
 
-// const More = ({ type }: ListProps) => {
-//   const { total, query, setQuery } = useContext(SiteListContext);
-//   const handlePageChange = (page: number) => {
-//     setQuery(
-//       Object.assign({}, query, {
-//         page,
-//       })
-//     );
-//   };
-//   const handleSizeChange = (size: number) => {
-//     setQuery(
-//       Object.assign({}, query, {
-//         size,
-//       })
-//     );
-//   };
-//   if (type === "home") {
-//     return (
-//       <Link href="/find">
-//         <a className={styles.more}>发现更多</a>
-//       </Link>
-//     );
-//   }
-//   return (
-//     <Pagination
-//       total={total}
-//       page={query.page}
-//       size={query.size}
-//       onPage={handlePageChange}
-//       onSize={handleSizeChange}
-//     />
-//   );
-// };
+const More = ({ type, size, total, page }: ListProps) => {
+  //   const { total, query, setQuery } = useContext(SiteListContext);
+  const handlePageChange = (page: number) => {
+    // setQuery(
+    //   Object.assign({}, query, {
+    //     page,
+    //   })
+    // );
+  };
+  const handleSizeChange = (size: number) => {
+    // setQuery(
+    //   Object.assign({}, query, {
+    //     size,
+    //   })
+    // );
+  };
+  if (type === 'home') {
+    return (
+      <Link href="/site">
+        <a className={styles.more}>发现更多</a>
+      </Link>
+    );
+  }
+  return (
+    <Pagination
+      total={total}
+      page={page}
+      size={size}
+      onPage={handlePageChange}
+      onSize={handleSizeChange}
+    />
+  );
+};
 
 const List = ({ type }: ListProps) => {
   const { data } = useSitesQuery({});
   const list = data?.list || [];
+  const { size = 20, page = 1 } = data?.pagination || {};
+  const total = data?.total || 0;
   return (
     <section className={styles.container}>
       <section className={styles.main}>
@@ -56,7 +61,7 @@ const List = ({ type }: ListProps) => {
             </li>
           ))}
         </ul>
-        {/* <More type={type} /> */}
+        <More type={type} total={total} size={size} page={page} />
       </section>
     </section>
   );
