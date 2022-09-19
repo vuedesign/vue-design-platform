cf215c268f5ea93df9e69686b31b28d21de324d2
n.see <gooarter@gmail.com>
2022-09-19 23:41:20 +0800

    feat(server-web): 新增 csxf插件

--- packages/server-web/src/core/middleware.ts
+++ packages/server-web/src/core/middleware.ts
@@ -3,13 +3,13 @@ import { INestApplication } from '@nestjs/common';
 import * as session from 'express-session';
 import * as cookieParser from 'cookie-parser';
 import * as compression from 'compression';
+import * as csurf from 'csurf';
 import { ConfigService } from '@nestjs/config';
 
 export default (app: INestApplication) => {
     const config = app.get(ConfigService);
     app.use(session(config.get('SESSION')));
-
     app.use(cookieParser());
-
+    app.use(csurf());
     app.use(compression());
 };
