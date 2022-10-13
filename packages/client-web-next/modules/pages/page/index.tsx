4909674d2b5f1635c3c5cffdba29ca2b077ad975
n.see <gooarter@gmail.com>
2022-10-13 23:24:07 +0800

    feat(page): markdown navbar

--- packages/client-web-next/modules/pages/page/index.tsx
+++ packages/client-web-next/modules/pages/page/index.tsx
@@ -72,6 +72,7 @@ export const getServerSideProps = wrapper.getServerSideProps(
 
 type TreeNode = {
     text: string;
+    hLevel: number;
     level: number;
     index: number;
     children?: TreeNode[];
@@ -79,35 +80,85 @@ type TreeNode = {
 
 function getHTrees(container: HTMLDivElement): TreeNode[] {
     const h = container.querySelectorAll('h2,h3,h4,h5');
-    const minLevels = new Set<number>([]);
     const list = Array.from(h).map((item, index) => {
-        const level = Number(item.tagName.match(/[0-9]/g));
-        minLevels.add(level);
+        const hLevel = Number(item.tagName.match(/[0-9]/g));
         return {
             text: item.innerHTML,
-            level,
+            hLevel,
+            level: 0,
             index,
         };
     });
-    const minLevel = Math.min(...Array.from(minLevels));
-    const maxLevel = Math.max(...Array.from(minLevels));
-    const offset = -Math.min(...Array.from(minLevels));
-    const result: TreeNode[] = [];
-    const levels: TreeNode[] = new Array(maxLevel - minLevel).fill({
-        children: result,
-    });
-    console.log('list', list.length);
-    list.forEach(function (o) {
-        levels[o.level + offset].children =
-            levels[o.level + offset].children || [];
 
-        levels[o.level + offset + 1] = o;
+    function toTree(flatArr: TreeNode[]) {
+        const tree: TreeNode[] = [];
+        const copyArr = flatArr.map((item) => {
+            return item;
+        });
 
-        (levels[o.level + offset].children as TreeNode[]).push(o);
-    });
+        // 根据指定级别查找该级别的子孙级，并删除掉已经查找到的子孙级
+        const getChildrenByLevel = (
+            currentLevelItem: TreeNode,
+            arr: TreeNode[],
+            level: number,
+        ) => {
+            if (!currentLevelItem) {
+                return [];
+            }
+            // 将level值转成负数，再进行比较
+            const minusCurrentLevel = -currentLevelItem.hLevel;
+            const children = [];
+            for (let i = 0, len = arr.length; i < len; i++) {
+                const levelItem = arr[i];
+                if (-levelItem.hLevel < minusCurrentLevel) {
+                    children.push(levelItem);
+                } else {
+                    // 只找最近那些子孙级
+                    break;
+                }
+            }
+            // 从数组中删除已经找到的那些子孙级，以免影响到其他子孙级的查找
+            if (children.length > 0) {
+                arr.splice(0, children.length);
+            }
+            return children;
+        };
 
-    console.log('h2', result);
-    // console.log('h3', h3);
+        const getTree = function (
+            result: TreeNode[],
+            arr: TreeNode[],
+            level: number,
+        ) {
+            // 首先将数组第一位移除掉，并添加到结果集中
+            let currentItem = arr.shift() as TreeNode;
+            currentItem.level = level;
+            result.push(currentItem);
+            while (arr.length > 0) {
+                if (!currentItem) {
+                    return;
+                }
+                // 根据当前级别获取它的子孙级
+                const children = getChildrenByLevel(currentItem, arr, level);
+                // 如果当前级别没有子孙级则开始下一个
+                if (children.length === 0) {
+                    currentItem = arr.shift() as TreeNode;
+                    currentItem.level = level;
+                    if (currentItem) {
+                        result.push(currentItem);
+                    }
+                    continue;
+                }
+                currentItem.children = [];
+                // 查找到的子孙级继续查找子孙级
+                getTree(currentItem.children, children, level + 1);
+            }
+        };
+        getTree(tree, copyArr, 1);
+        return tree;
+    }
+
+    const result = toTree(list);
+    console.log('result h:', result);
     return result;
 }
 
