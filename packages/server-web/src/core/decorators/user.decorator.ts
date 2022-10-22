3a53322c0a3ec3504c7c08fa737437b0dac0a5aa
n.see <gooarter@gmail.com>
2022-10-22 20:47:32 +0800

    fix(server-web): 用户的登录bug

--- packages/server-web/src/core/decorators/user.decorator.ts
+++ packages/server-web/src/core/decorators/user.decorator.ts
@@ -3,9 +3,9 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const User = createParamDecorator(
     (data: string, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest();
-        if (!request.cookies || !request.cookies.token) {
-            return null;
-        }
+        // if (!request.cookies || !request.cookies.token) {
+        //     return null;
+        // }
         if (data && request.user) {
             return request.user[data];
         }
